1c4e2f51ba180f8e865202117b3e4389f78a7f36
Anjali Jha <23anjalijha@gmail.com>
2026-06-25 21:42:00 +0530

    Create random_words.py

---
+++ random_words.py
@@ -0,0 +1,12 @@
+words = [
+    "adventure", "anxiety", "astronaut", "backpack", "beautiful", 
+    "blizzard", "breeze", "broadcast", "bumblebee", "catastrophe", 
+    "chameleon", "clergyman", "dashboard", "dazzling", "dinosaur", 
+    "disaster", "eclipse", "electricity", "elements", "explorer", 
+    "firecracker", "flamingo", "galaxy", "gargoyle", "gorgon", 
+    "grizzly", "hangman", "horizon", "hyphen", "iceberg", 
+    "infinite", "islander", "javelin", "journey", "jungle", 
+    "kingdom", "labyrinth", "leopard", "lightning", "marathon", 
+    "mysterious", "navigator", "network", "octopus", "olympics", 
+    "orchard", "ostrich", "overwhelmed", "oxygen", "paradise"
+]
